f06adf81427ecfcdfd31af07c4dd2911ca7c7280
lucien <90026403+lucy319@users.noreply.github.com>
2022-09-20 16:52:41 +0530

    Added validate method for login validation
    returns true if the password and username are true

--- SaveReg/src/main/java/dao/EmployeeDao.java
+++ SaveReg/src/main/java/dao/EmployeeDao.java
@@ -1,5 +1,9 @@
 package dao;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import model.Employee;
 public class EmployeeDao {
@@ -27,5 +31,28 @@ public int registerEmplyoee(Employee e)throws ClassNotFoundException, SQLExcepti
 	    }
 	
 	return result;
+}
+public boolean validate(String uname,String upass) throws ClassNotFoundException {
+	String prep="select * from employee where `user name`=? and password=?";
+	String url="jdbc:mysql://localhost:3306/employees";
+	String name="root";
+	String pass="mysql";
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	try(Connection con=DriverManager.getConnection(url,name,pass)){
+	    PreparedStatement ps=con.prepareStatement(prep);
+	    ps.setString(1,uname);
+	    ps.setString(2,upass);
+	    ResultSet re=ps.executeQuery();
+	    if(re.next()) {
+	    	 System.out.println("valid user");
+	    	 return true;
+	    }
+	   
+	    
+	}
+	catch(Exception ee) {
+		ee.getMessage();
+	    }
+	return false;
 }
 }
